2b758c8abc05db6b54f3d33fe549d94f18f5e928
Edin-K <edin.korkic@hotmail.com>
2018-02-09 22:52:59 +0100

    Second commit

---
+++ src/zadaca11/CountLetters.java
@@ -0,0 +1,42 @@
+package zadaca11;
+//Zadaca_11_16_11_2017
+
+import java.util.Scanner;
+
+/*
+ * @author Edin Korkic
+ *
+ *  Napisati metodu koja broji slova u stringu. Metoda treba koristiti sljedeæi header: 
+ *  public static int prebrojiSlova(String s). Napisati program koji pita korisnika da 
+ *  unese neki string te mu vrati koliko taj string ima slova. 
+ */
+
+public class CountLetters {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print(" Unesite string: ");
+
+		String s = input.nextLine();
+
+		input.close();
+
+		System.out.printf(" String koji ste unijeli ima %d slova.", prebrojiSlova(s));
+	}
+
+	public static int prebrojiSlova(String s) {
+
+		int count = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			if (Character.isLetter(s.charAt(i))) {
+				count++;
+			}
+
+		}
+		return count;
+
+	}
+
+}
